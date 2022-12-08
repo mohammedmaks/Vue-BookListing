17ebeb250bc9c1c6fe5fff643f947a8b2b1d8256
mohammedmaks <mohammedmaks16@gmail.com>
2022-12-08 12:39:18 +0000

    Complete Module 4

---
+++ src/components/BookItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <ul>
+            <li>{{ book.title }}: {{ book.author }}</li>
+            <li v-for="book in books">{{ book.title }}: {{ book.author }}</li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'BookItem',
+    props: ['book'],
+};
+
+</script>
+
+<style>
+li {
+    display: block;
+    margin: 0 10px;
+}
+</style>
